615039b47fd99cedd001bc3ba239b4d76eef1f6c
Eric Heinzl <xpand.4beatz@gmail.com>
2021-11-23 18:01:48 +0100

    Renamed structures to avoid name conflicts

--- src/main/java/waldinet/towers_of_the_wild_reworked/utils/StructUtils.java
+++ src/main/java/waldinet/towers_of_the_wild_reworked/utils/StructUtils.java
@@ -22,7 +22,7 @@ public class StructUtils
     public static final Identifier JUNGLE_TOWER = TowersOfTheWildReworked.id("jungle_tower");
     public static final Identifier OCEAN_TOWER = TowersOfTheWildReworked.id("ocean_tower");
     public static final Identifier OCEAN_WARM_TOWER = TowersOfTheWildReworked.id("ocean_warm_tower");
-    public static final Identifier REGULAR_TOWER = TowersOfTheWildReworked.id("regular_tower");
+    public static final Identifier REGULAR_TOWER = TowersOfTheWildReworked.id("tower");
     //#endregion
 
     /**
